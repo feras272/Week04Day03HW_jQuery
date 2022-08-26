4dfd711d229282c3ae9537431b1beccf5541ad19
feras alnowiser <ferasdev@gmail.com>
2022-08-26 20:43:29 +0300

    adding the ready function for jQuery in main.js

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 console.log('Tuwaiq Academy - JavaScript')
 console.log('Week 4 Day 3 HW')
 
-const users = [{user:'Feras', password:'1234'}, {user:'Alnowiser', password:'5678'}];
+$(document).ready(function () {
+
+    const users = [{user:'Feras', password:'1234'}, {user:'Alnowiser', password:'5678'}];
+    
+})
