c3f61027c2358a232b3ca6424b6416169e7582d5
feras alnowiser <ferasdev@gmail.com>
2022-08-26 20:49:48 +0300

    adding style to the main.js using jQuery

--- main.js
+++ main.js
@@ -4,5 +4,10 @@ console.log('Week 4 Day 3 HW')
 $(document).ready(function () {
 
     const users = [{user:'Feras', password:'1234'}, {user:'Alnowiser', password:'5678'}];
-    
+
+    function verify() {
+        $('#loginPa').css("background-color");
+        
+    }
+
 })
