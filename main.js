a184eb60feb3e46e4ea52f9648e58e02d03feaf9
feras alnowiser <ferasdev@gmail.com>
2022-08-27 13:52:40 +0300

    adding the HTML elements inside jQuery at main.js

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ console.log('Week 4 Day 3 HW')
 $(document).ready(function () {
 
     const users = [{user:'Feras', password:'1234'}, {user:'Alnowiser', password:'5678'}];
+    const username = $('#username');
+    const userpass = $('#password');
+    const btn_login = $('#login');
+    const par_login = $('#loginPar');
 
     function verify() {
         $('#loginPa').css("background-color");
